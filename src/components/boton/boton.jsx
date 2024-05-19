383dede3fbbaf0371f2f6b45fc6c0ce83a15686e
mcmarin21 <carlitosmarin365@gmail.com>
2024-05-18 23:35:52 -0600

    creacion componente boton

---
+++ src/components/boton/boton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './boton.css';
+export default function Instructions(props) {
+    return(
+        <button className={props.estilo}>
+            <p>hasdjasd</p>
+        </button>
+    )
+}
