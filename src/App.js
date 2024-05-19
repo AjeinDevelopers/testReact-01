9ff1103a3fdd1df67ee3625ca84d56eb21db3116
mcmarin21 <carlitosmarin365@gmail.com>
2024-05-19 12:37:19 -0600

    Actualizacion boton y agregada la posibilidad de deploy

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Boton from "./components/boton/boton.jsx";
 function App() {
   return(
       <div className="container" style={{margin: 64}}>
-
-          <Boton estilo={"secondary"} />
-
+          <Boton estilo={"secondary"} text={"Botón"} showIcon1={true} icon1={"fa-solid fa-right-to-bracket fa-fw"} showIcon2={true} icon2={"fa-solid fa-right-to-bracket fa-fw"} handleClick={clicksito}/>
       </div>
   )
+}
+
+function clicksito(){
+    window.alert("Hola");
 }
 
 export default App;
--- src/components/boton/boton.jsx
+++ src/components/boton/boton.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import './boton.css';
 export default function Instructions(props) {
     return(
-        <button className={props.estilo}>
-            <p>hasdjasd</p>
+        <button className={props.estilo} onClick={props.handleClick}>
+            {props.showIcon1 && <i className={props.icon1}></i>}
+            <t5>{props.text}</t5>
+            {props.showIcon2 && <i className={props.icon2}></i>}
         </button>
     )
 }
